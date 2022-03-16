e97339798507775f50aa04c90024e088cad89be5
KOH <gaosheng087@gmail.com>
2022-03-16 12:08:45 +0900

    Javascript変更

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 //SVGアニメーションの描画
 var stroke;
-stroke = new Vivus('mask', {//アニメーションをするIDの指定
-    start:'manual',//自動再生をせずスタートをマニュアルに
-    type: 'scenario-sync',// アニメーションのタイプを設定
-    duration: 80,//アニメーションの時間設定。数字が小さくなるほど速い
-    forceRender: false,//パスが更新された場合に再レンダリングさせない
-    animTimingFunction:Vivus.EASE,//動きの加速減速設定
+stroke = new Vivus('mask', {
+    start:'manual',
+    type: 'scenario-sync',
+    duration: 80,
+    forceRender: false,
+    animTimingFunction:Vivus.EASE,
 }
 );
 
 $(window).on('load',function(){
-    $("#splash").delay(1800).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェイドアウト
-    $("#splash_logo").delay(1800).fadeOut('slow');//ロゴを1.5秒（1500ms）待機してからフェイドアウト
-        stroke.play();//SVGアニメーションの実行
+    $("#splash").delay(1800).fadeOut('slow');
+    $("#splash_logo").delay(1800).fadeOut('slow');
+        stroke.play();
 });
