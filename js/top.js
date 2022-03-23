34eb9148518d02a1ff4d90316f9efe8363b57b03
KOH <gaosheng087@gmail.com>
2022-03-24 00:18:59 +0900

    最終編集

--- js/top.js
+++ js/top.js
@@ -1,6 +1,5 @@
 //SVGアニメーションの描画
-var stroke;
-stroke = new Vivus('mask', {
+const stroke = new Vivus('mask', {
     start:'manual',
     type: 'scenario-sync',
     duration: 80,
@@ -10,7 +9,7 @@ stroke = new Vivus('mask', {
 );
 
 $(window).on('load',function(){
-    $("#splash").delay(1800).fadeOut('slow');
-    $("#splash_logo").delay(1800).fadeOut('slow');
+    $("#splash").delay(2000).fadeOut('slow');
+    $("#splash_logo").delay(2000).fadeOut('slow');
         stroke.play();
 });
